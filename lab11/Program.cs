e24eff98862a1edfc1ecbb4dde66937f95311fdd
Dsniil Votintsev <git config --global user.email votintsev.danill@ya.ru>
2024-03-20 23:30:45 +0500

    Add project files.

---
+++ lab11/Program.cs
@@ -0,0 +1,371 @@
+﻿using CustomLibrary;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab11
+{
+    public class MyCollection
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАНИЕ 1
+
+            Hashtable hashTable = new Hashtable();
+            //Инициализация
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    SmileEmoji e = new SmileEmoji();
+                    e.RandomInit();
+                    Emoji emoji1 = new Emoji(e.Name, e.Tag, e.id.number);
+
+                    hashTable.Add(emoji1, e);
+                }
+                catch (Exception e)
+                {
+                    i--;
+                }
+            }
+
+            //вывод элементов
+            foreach (var item in hashTable.Keys)
+            {
+                Console.WriteLine($"Ключ: {item}\nЗначение: {hashTable[item]} ");
+            }
+
+            //добавление
+            Console.WriteLine("Введите элемент для добавления в таблицу");
+            SmileEmoji newEmoji = new SmileEmoji();
+            newEmoji.Init();
+            try
+            {
+                Emoji key = new Emoji(newEmoji.Name, newEmoji.Tag, newEmoji.id.number);
+                hashTable.Add(key, newEmoji);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Такой элемент уже есть, добавление невозможно");
+            }
+
+            //поиск
+            Console.WriteLine("Введите элемент для поиска:");
+            Emoji smile = new Emoji();
+            smile.Init();
+
+            if (hashTable.ContainsKey(smile))
+            {
+                Console.WriteLine("Элемент найден");
+            }
+            else
+                Console.WriteLine("Такого элемента нет");
+
+            //удаление
+            Console.WriteLine("Введите значение ключа удаляемого элемента");
+            Emoji target = new Emoji();
+            target.Init();
+
+            if (hashTable.ContainsKey(target))
+            {
+                hashTable.Remove(target);
+                Console.WriteLine("Элемент удален");
+            }
+            else
+                Console.WriteLine("Такого элемента нет");
+
+            //клонирование
+            Hashtable deepClone = new Hashtable();
+
+            foreach (var item in hashTable.Keys)
+            {
+                // Получаем ключ и значение из оригинальной Hashtable
+                Emoji originalKey = (Emoji)item;
+                SmileEmoji originalValue = (SmileEmoji)hashTable[item];
+
+                // Клонируем ключ и значение
+                Emoji clonedKey = (Emoji)originalKey.Clone();
+                SmileEmoji clonedValue = (SmileEmoji)originalValue.Clone();
+
+                // Добавляем клонированные объекты в новую Hashtable
+                deepClone.Add(clonedKey, clonedValue);
+            }
+
+            // Выводим клонированную Hashtable
+            Console.WriteLine("Клон:");
+            foreach (DictionaryEntry entry in deepClone)
+            {
+                Console.WriteLine($"Ключ: {entry.Key}\nЗначение: {entry.Value}");
+            }
+
+            //сортировка
+
+            /// Создаем копию ключей
+            ArrayList keys = new ArrayList(hashTable.Keys);
+            // Сортируем список ключей
+            keys.Sort();
+
+            // Создаем новую Hashtable для хранения отсортированных элементов
+            Hashtable sortedHashTable = new Hashtable();
+
+            // Для каждого отсортированного ключа добавляем значение из исходной Hashtable в новую Hashtable
+            foreach (var key in keys)
+            {
+                sortedHashTable.Add(key, hashTable[key]);
+            }
+
+            Console.WriteLine("Сортированная копия Hashtable:");
+            foreach (var entry in sortedHashTable.Keys)
+            {
+                Console.WriteLine($"Ключ: {entry}\nЗначение: {sortedHashTable[entry]}");
+            }
+
+            Console.WriteLine(FindCountOfSmile(hashTable));
+
+            PrintSmile(hashTable);
+
+            //ЗАДАНИЕ 2
+
+            //инициализация
+            Queue<Emoji> queue = new Queue<Emoji>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                SmileEmoji smileEmoji = new SmileEmoji();
+                smileEmoji.RandomInit();
+
+                queue.Enqueue(smileEmoji);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                FaceEmoji faceEmoji = new FaceEmoji();
+                faceEmoji.RandomInit();
+
+                queue.Enqueue(faceEmoji);
+            }
+            foreach (Emoji item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            //поиск
+            Console.WriteLine("введите параметры поиска");
+            Emoji find = new Emoji();
+            find.Init();
+
+            if (queue.Contains(find))
+            {
+                Console.WriteLine("такой объект есть");
+            }
+            else
+                Console.WriteLine("Объект не найден");
+
+            //удаление
+            Console.WriteLine("введите параметры удаления");
+            Emoji target1 = new Emoji();
+            Queue<Emoji> temp = new Queue<Emoji>();
+
+            if (queue.Contains(target1))
+            {
+                foreach (Emoji item in queue)
+                {
+                    if (item != target1)
+                    {
+                        temp.Enqueue(item);
+                    }
+                }
+                queue.Clear();
+
+                foreach (Emoji item in temp)
+                {
+                    queue.Enqueue(item);
+                }
+
+            }
+
+            //сортировка
+
+            // Сортируем элементы очереди
+            Emoji[] sortedArray = queue.ToArray();
+            Array.Sort(sortedArray);
+
+            // Создаем новую очередь с отсортированными элементами
+            Queue<Emoji> sortedQueue = new Queue<Emoji>(sortedArray);
+
+            // Выводим отсортированные элементы
+            Console.WriteLine("Отсортированная очередь:");
+            foreach (var item in sortedQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            //добавление в лист
+            Console.WriteLine("Введите элемент для добавления:");
+            Emoji emoji = new Emoji();
+            emoji.Init();
+
+            queue.Enqueue(emoji);
+
+            //клонирование
+            Queue<Emoji> queueClone = new Queue<Emoji>();
+            Queue<Emoji> temp1 = new Queue<Emoji>();
+
+            foreach (var item in queue)
+            {
+                temp1.Enqueue(item);
+            }
+
+            foreach (var item in temp1)
+            {
+                if (item is SmileEmoji e)
+                {
+                    SmileEmoji s = (SmileEmoji)e.Clone();
+                    queueClone.Enqueue(s);
+                }
+                if (item is AnimalEmoji a)
+                {
+                    AnimalEmoji s = (AnimalEmoji)a.Clone();
+                    queueClone.Enqueue(s);
+                }
+                if (item is FaceEmoji f)
+                {
+                    FaceEmoji s = (FaceEmoji)f.Clone();
+                    queueClone.Enqueue(s);
+                }
+                if (item is Emoji em)
+                {
+                    Emoji s = (Emoji)em.Clone();
+                    queueClone.Enqueue(s);
+                }
+
+            }
+
+            //демонстрация
+            foreach (var item in queueClone)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(FindCountOfAnimal(queue));
+
+            PrintSmile(queue);
+
+            PrintFaceTag(queue);
+
+            //ЗАДАНИЕ 3
+
+            TestCollections test = new TestCollections();
+            Console.WriteLine("ok");
+
+            test.Get1Time();
+            Console.WriteLine("");
+            test.Get2Time();
+            Console.WriteLine("");
+            test.Get3Time();
+            Console.WriteLine("");
+            test.Get4Time();
+        }
+
+        //ФУНКЦИИ ЗАДАНИЯ 1
+
+        //подсчет количества улыбающихся смайлов
+        static string FindCountOfSmile(Hashtable ht)
+        {
+            int count = 0;
+            foreach (var i in ht.Values)
+            {
+                if (i is SmileEmoji)
+                {
+                    count++;
+                }
+            }
+            return ($"В листе {count} улыбыющихся эмоджи");
+        }
+
+        //вывод всех улыбающихся со степенью больше 5
+        static void PrintSmile(Hashtable ht)
+        {
+            int count = 0;
+            foreach (var i in ht.Values)
+            {
+                if (i is SmileEmoji s && s.Grade>=5)
+                {
+                    Console.WriteLine(s);
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("В листе нет улыбающихся эмоджи.");
+        }
+
+        //вывод тэгов улыбающихся со степенью <3
+        static void PrintTag(Hashtable ht)
+        {
+            int count = 0;
+            foreach (var i in ht.Values)
+            {
+                if (i is SmileEmoji e && e.Grade<=3)
+                {
+                    Console.WriteLine(e.Tag);
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("В листе нет SmileEmoji");
+        }
+
+        //ФУНКЦИИ ЗАДАНИЯ 2
+
+        //подсчет количества животных
+        static string FindCountOfAnimal(Queue<Emoji> queue)
+        {
+            int count = 0;
+            foreach (var i in queue)
+            {
+                if (i is AnimalEmoji)
+                {
+                    count++;
+                }
+            }
+            return ($"В листе {count} животных - эмоджи");
+        }
+
+        //вывод всех улыбающихся
+        static void PrintSmile(Queue<Emoji> list)
+        {
+            int count = 0;
+            foreach (var i in list)
+            {
+                if (i is SmileEmoji s)
+                {
+                    Console.WriteLine(s);
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("В листе нет улыбающихся эмоджи.");
+        }
+
+        //поиск всех тэгов животных
+        static void PrintFaceTag(Queue<Emoji> list)
+        {
+            int count = 0;
+            foreach (var i in list)
+            {
+                if (i is FaceEmoji)
+                {
+                    Console.WriteLine(i.Tag);
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("В листе нет FaceEmoji");
+        }
+
+       
+    }
+}
---
+++ lab11/TestCollections.cs
@@ -0,0 +1,318 @@
+﻿using CustomLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections;
+
+namespace lab11
+{
+    internal class TestCollections
+    {
+        public Stack<Emoji> stack1;
+        public Stack<string> stack2;
+        public Dictionary<Emoji, AnimalEmoji> dictionary1;
+        public Dictionary<string, AnimalEmoji> dictionary2;
+
+        public TestCollections()
+        {
+            stack1 = new Stack<Emoji>();
+            for (int i = 0; i < 1000; i++)
+            {
+                AnimalEmoji e = new AnimalEmoji();
+                e.RandomInit();
+                stack1.Push(e);
+            }
+
+            stack2 = new Stack<string>();
+            for (int i = 0; i < 1000; i++)
+            {
+                AnimalEmoji e = new AnimalEmoji();
+                e.RandomInit();
+                stack2.Push(e.ToString());
+            }
+
+            dictionary1 = new Dictionary<Emoji, AnimalEmoji>();
+            for(int i = 0;i < 1000; i++)
+            {
+                try
+                {
+                    AnimalEmoji animalEmoji = new AnimalEmoji();
+                    animalEmoji.RandomInit();
+                    Emoji emoji = new Emoji(animalEmoji.Name, animalEmoji.Tag, animalEmoji.id.number);
+                    dictionary1.Add(emoji, animalEmoji);
+                }
+                catch(Exception ex)
+                {
+                    i--;
+                }
+            }
+
+            dictionary2 = new Dictionary<string, AnimalEmoji>();
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    AnimalEmoji animalEmoji = new AnimalEmoji();
+                    animalEmoji.RandomInit();
+                    Emoji emoji = new Emoji(animalEmoji.Name, animalEmoji.Tag, animalEmoji.id.number);
+                    dictionary2.Add(emoji.ToString(), animalEmoji);
+                }
+                catch (Exception ex)
+                {
+                    i--;
+                }
+            }
+        }
+
+        public void Get1Time()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            // Получение первого элемента (последнего добавленного элемента)
+            Emoji firstElement = stack1.Peek();
+
+            // Получение последнего элемента (первого добавленного элемента)
+            Emoji lastElement = stack1.ToArray()[0];
+
+            // Получение центрального элемента
+            Emoji centralElement = stack1.ToArray()[stack1.Count / 2];
+
+            long all = 0;
+
+            Console.WriteLine("Время поиска (среднее за 100 замеров) в стеке 1");
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+                stack1.Contains(firstElement);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            Console.WriteLine($"Первый найден за {(double)all / 100}");
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+                stack1.Contains(lastElement);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            Console.WriteLine($"Последний найден за {(double)all / 100}");
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+                stack1.Contains(lastElement);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            Console.WriteLine($"Средний найден за {(double)all / 100}");
+        }
+
+        public void Get2Time()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            // Получение первого элемента (последнего добавленного элемента)
+            string firstElement = stack2.Peek();
+
+            // Получение последнего элемента (первого добавленного элемента)
+            string lastElement = stack2.ToArray()[0];
+
+            // Получение центрального элемента
+            string centralElement = stack2.ToArray()[stack1.Count / 2];
+
+            long all = 0;
+
+            Console.WriteLine("Время поиска (среднее за 100 замеров) в стеке 2");
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+                stack2.Contains(firstElement);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            Console.WriteLine($"Первый найден за {(double)all / 100}");
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+                stack2.Contains(lastElement);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            Console.WriteLine($"Последний найден за {(double)all / 100}");
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+                stack2.Contains(lastElement);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            Console.WriteLine($"Средний найден за {(double)all / 100}");
+        }
+
+        public void Get3Time()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Emoji firstKey = dictionary1.Keys.FirstOrDefault();
+            Emoji lastKey = dictionary1.Keys.LastOrDefault();
+            Emoji midKey = dictionary1.Keys.ElementAt(dictionary1.Count/2);
+            long all = 0;
+            bool ok1 = true;
+
+            Console.WriteLine("Среднее время поиска по ключу(100 замеров) в словаре 1:");
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+
+                // Проверяем наличие первого ключа в словаре
+                ok1 = dictionary1.ContainsKey(firstKey);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+
+            if (ok1)
+            {
+                Console.WriteLine($"Первый найден за {(double)all / 100}");
+            }
+            else
+            {
+                Console.WriteLine($"Первый не найден за {(double)all / 100}");
+            }
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+
+                // Проверяем наличие первого ключа в словаре
+                ok1 = dictionary1.ContainsKey(lastKey);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            if (ok1)
+            {
+                Console.WriteLine($"Последний найден за {(double)all / 100}");
+            }
+            else
+            {
+                Console.WriteLine($"Последний не найден за {(double)all / 100}");
+            }
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+
+                // Проверяем наличие первого ключа в словаре
+                ok1 = dictionary1.ContainsKey(midKey);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            if (ok1)
+            {
+                Console.WriteLine($"В середине найден за {(double)all / 100}");
+            }
+            else
+            {
+                Console.WriteLine($"В середине не найден за {(double)all / 100}");
+            }
+
+        }
+
+        
+
+        public void Get4Time()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            string firstKey = ((Emoji)dictionary1.Values.FirstOrDefault()).ToString();
+            string lastKey = ((Emoji)dictionary1.Values.LastOrDefault()).ToString();
+            string midKey = ((Emoji)dictionary1.Values.ElementAt(dictionary1.Count / 2)).ToString();
+            long all = 0;
+            bool ok1 = true;
+
+            Console.WriteLine("Среднее время поиска по ключу(100 замеров) в словаре 2:");
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+
+                // Проверяем наличие первого ключа в словаре
+                ok1 = dictionary2.ContainsKey(firstKey);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+
+            if (ok1)
+            {
+                Console.WriteLine($"Первый найден за {(double)all / 100}");
+            }
+            else
+            {
+                Console.WriteLine($"Первый не найден за {(double)all / 100}");
+            }
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+
+                // Проверяем наличие первого ключа в словаре
+                ok1 = dictionary2.ContainsKey(lastKey);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            if (ok1)
+            {
+                Console.WriteLine($"Последний найден за {(double)all / 100}");
+            }
+            else
+            {
+                Console.WriteLine($"Последний не найден за {(double)all / 100}");
+            }
+            all = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                sw.Restart();
+
+                // Проверяем наличие первого ключа в словаре
+                ok1 = dictionary2.ContainsKey(midKey);
+                sw.Stop();
+
+                all += sw.ElapsedTicks; ;
+            }
+            if (ok1)
+            {
+                Console.WriteLine($"В середине найден за {(double)all / 100}");
+            }
+            else
+            {
+                Console.WriteLine($"В середине не найден за {(double)all / 100}");
+            }
+
+        }
+    }
+}
